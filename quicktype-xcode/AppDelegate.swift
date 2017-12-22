993b97a485aa0e1fd4724222929d2d7b621897a5
David Siegel <djsiegel@gmail.com>
2017-12-21 20:47:27 -0800

    Initial Commit

---
+++ quicktype-xcode/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  quicktype-xcode
+//
+//  Created by David Siegel on 12/21/17.
+//  Copyright © 2017 quicktype. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
