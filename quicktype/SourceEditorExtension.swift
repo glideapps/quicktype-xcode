24eb7bfdbb6fed05072d275f6684d8a6512dced7
David Siegel <djsiegel@gmail.com>
2017-12-22 11:05:59 -0800

    clean

--- quicktype/SourceEditorExtension.swift
+++ quicktype/SourceEditorExtension.swift
@@ -1,27 +1,8 @@
-//
-//  SourceEditorExtension.swift
-//  quicktype
-//
-//  Created by David Siegel on 12/21/17.
-//  Copyright © 2017 quicktype. All rights reserved.
-//
-
 import Foundation
 import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
-    
-    /*
     func extensionDidFinishLaunching() {
-        // If your extension needs to do any work at launch, implement this optional method.
+        Runtime.shared?.intialize()
     }
-    */
-    
-    /*
-    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
-        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-        return []
-    }
-    */
-    
 }
